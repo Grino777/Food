b8bde25c62f90e931be8ec492c39978f6a1f683d
Grino777 <bars_B_P@mail.ru>
2023-10-14 14:56:37 +0300

    Add Slider indicators

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -282,6 +282,7 @@ window.addEventListener('DOMContentLoaded', function () {
     const showSliderCounter = async () => {
         total.textContent = await checkDigit(quantity);
         current.textContent = await checkDigit(counter);
+        highlightIndicator(counter);
     };
 
     const setSlideField = async () => {
@@ -305,6 +306,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
         current.textContent = await checkDigit(counter);
         slidesField.style.transform = `translateX(${offset}px)`;
+        highlightIndicator(counter);
     });
 
     prev.addEventListener('click', async () => {
@@ -317,9 +319,59 @@ window.addEventListener('DOMContentLoaded', function () {
         }
         current.textContent = await checkDigit(counter);
         slidesField.style.transform = `translateX(${offset}px)`;
+        highlightIndicator(counter);
     });
 
     showSliderCounter();
     addSlidesOnPage(data);
     setSlideField();
+
+    //Slider Carousel
+
+    function createSliderCarousel() {
+        const carousel = document.createElement('ol'),
+            offerSlider = document.querySelector('.offer__slider');
+
+        offerSlider.style.position = 'relative';
+        carousel.classList.add('carousel-indicators');
+        offerSlider.append(carousel);
+    }
+
+    async function addSliderIndicators() {
+        for (let i = 0; i < (await quantity); i++) {
+            const dot = document.createElement('li');
+            dot.classList.add('dot');
+            dot.setAttribute('data-number', i + 1);
+
+            dot.addEventListener('click', (event) => {
+                const target = event.target,
+                    number = +target.getAttribute('data-number');
+
+                highlightIndicator(number);
+            });
+
+            document.querySelector('.carousel-indicators').append(dot);
+        }
+    }
+
+    function removeHighlight() {
+        const indicators = document.querySelectorAll(`[data-number]`);
+        indicators.forEach((indicator) => {
+            indicator.classList.remove('highlight');
+        });
+    }
+
+    async function highlightIndicator(slideNumber) {
+        removeHighlight();
+        const indicator = document.querySelector(`[data-number="${slideNumber}"]`);
+        indicator.classList.add('highlight');
+
+        offset = -((slideNumber - 1) * width);
+        counter = slideNumber;
+        current.textContent = await checkDigit(slideNumber);
+        slidesField.style.transform = `translateX(${offset}px)`;
+    }
+
+    createSliderCarousel();
+    addSliderIndicators();
 });
